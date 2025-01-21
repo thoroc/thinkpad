89e471bda95fbf2edbd2ee5976f6634cb7a41073
thoroc <thomas.a.roche@gmail.com>
2025-01-21 12:06:52 +0000

    docs: getFileConfig

--- opt/scripts/src/file/config.ts
+++ opt/scripts/src/file/config.ts
@@ -6,6 +6,17 @@ export type FileConfig = {
   parentDir: string;
 };
 
+/**
+ * Extracts and returns the configuration details of a file from its filepath.
+ *
+ * @param filepath - The full path of the file.
+ * @returns An object containing the file's configuration details:
+ * - `extension`: The file extension.
+ * - `name`: The name of the file without the extension. Excludes any values after a period.
+ * - `path`: The directory path of the file. Excludes the filename.
+ * - `root`: The root directory of the file path.
+ * - `parentDir`: The parent directory of the file.
+ */
 export const getFileConfig = (filepath: string): FileConfig => {
   const pathParts = filepath.split("/");
   const nameParts = (pathParts.pop() as string).split(".");
