ae31ede9349a3fb2e2933e8104a35932bdd48b09
thoroc <thomas.a.roche@gmail.com>
2025-01-21 11:10:22 +0000

    extracted main function

--- data/scripts/main.ts
+++ data/scripts/main.ts
@@ -1,91 +1,9 @@
-import { existsSync } from "jsr:@std/fs";
-import chalk from "npm:chalk";
-import { convertXls } from "./src/convert.ts";
-import { writeExports } from "./src/exports.ts";
-import { renameFile } from "./src/rename.ts";
-import { generateSchemas } from "./src/schemas/mod.ts";
-import { ExcelFileExtension } from "./src/types.ts";
+import { generateTypes } from "./src/generate.ts";
 
-interface Options {
-  inputDir: string;
-  dataDir: string;
-  schemaDir: string;
-  fileExtension: ExcelFileExtension;
-  maxFiles?: number;
-}
-
-export const main = async (
-  { inputDir, dataDir, fileExtension, maxFiles }: Options,
-) => {
-  const files = Deno.readDirSync(inputDir);
-  let counter = 0;
-
-  if (!existsSync(dataDir)) {
-    Deno.mkdirSync(dataDir, { recursive: true });
-  }
-
-  if (!existsSync(schemaDir)) {
-    Deno.mkdirSync(schemaDir, { recursive: true });
-  }
-
-  const datafiles = [];
-
-  for (const file of files) {
-    if (maxFiles && counter >= maxFiles && maxFiles !== -1) {
-      break;
-    }
-
-    const filepath = `${inputDir}/${file.name}`;
-
-    console.log(`\n\n> Processing ${chalk.yellow(filepath)}\n`);
-
-    const xlsFilename = await renameFile({ file, fileExtension });
-
-    if (xlsFilename) {
-      const jsonFile = await convertXls({
-        filepath: `${inputDir}/${xlsFilename}`,
-        outputDir: dataDir,
-        exportFileExtension: "json",
-      });
-      datafiles.push(jsonFile);
-
-      counter++;
-    }
-
-    console.log(`\n> Done processing ${chalk.yellow(filepath)}`);
-  }
-
-  const T4Type = await generateSchemas({
-    inputFiles: datafiles,
-    filter: "T4",
-    outputDir: schemaDir,
-  });
-
-  const X2Type = await generateSchemas({
-    inputFiles: datafiles,
-    filter: "X2",
-    outputDir: schemaDir,
-  });
-
-  console.log(`\n\n> Generating exports`);
-
-  writeExports({
-    outputDir: schemaDir,
-    files: [...T4Type, ...X2Type],
-  });
-
-  console.log("\n\n> Done");
-};
-
-// get all files in the directory
-const inputDir = "data/imports";
-const dataDir = "data/clean";
-const schemaDir = "data/schemas";
-
-await main({
-  inputDir,
-  dataDir,
-  schemaDir,
+await generateTypes({
+  inputDir: "data/imports",
+  dataDir: "data/clean",
+  schemaDir: "data/schemas",
   fileExtension: "xls",
   maxFiles: -1,
 });
---
+++ data/scripts/src/generate.ts
@@ -0,0 +1,82 @@
+import { emptyDir, existsSync } from "jsr:@std/fs";
+import chalk from "npm:chalk";
+import { convertXls } from "./convert.ts";
+import { writeExports } from "./exports.ts";
+import { renameFile } from "./rename.ts";
+import { generateSchemas } from "./schemas/mod.ts";
+import { ExcelFileExtension } from "./types.ts";
+
+interface Options {
+  inputDir: string;
+  dataDir: string;
+  schemaDir: string;
+  fileExtension: ExcelFileExtension;
+  maxFiles?: number;
+}
+
+export const generateTypes = async (
+  { inputDir, dataDir, schemaDir, fileExtension, maxFiles }: Options,
+) => {
+  const files = Deno.readDirSync(inputDir);
+  let counter = 0;
+
+  if (!existsSync(dataDir)) {
+    Deno.mkdirSync(dataDir, { recursive: true });
+  } else {
+    emptyDir(dataDir);
+  }
+
+  if (!existsSync(schemaDir)) {
+    Deno.mkdirSync(schemaDir, { recursive: true });
+  } else {
+    emptyDir(schemaDir);
+  }
+
+  const datafiles = [];
+
+  for (const file of files) {
+    if (maxFiles && counter >= maxFiles && maxFiles !== -1) {
+      break;
+    }
+
+    const filepath = `${inputDir}/${file.name}`;
+
+    console.log(`\n\n> Processing ${chalk.yellow(filepath)}\n`);
+
+    const xlsFilename = await renameFile({ file, fileExtension });
+
+    if (xlsFilename) {
+      const jsonFile = await convertXls({
+        filepath: `${inputDir}/${xlsFilename}`,
+        outputDir: dataDir,
+        exportFileExtension: "json",
+      });
+      datafiles.push(jsonFile);
+
+      counter++;
+    }
+
+    console.log(`\n> Done processing ${chalk.yellow(filepath)}`);
+  }
+
+  const T4Type = await generateSchemas({
+    inputFiles: datafiles,
+    filter: "T4",
+    outputDir: schemaDir,
+  });
+
+  const X2Type = await generateSchemas({
+    inputFiles: datafiles,
+    filter: "X2",
+    outputDir: schemaDir,
+  });
+
+  console.log(`\n\n> Generating exports`);
+
+  writeExports({
+    outputDir: schemaDir,
+    files: [...T4Type, ...X2Type],
+  });
+
+  console.log("\n\n> Done");
+};
