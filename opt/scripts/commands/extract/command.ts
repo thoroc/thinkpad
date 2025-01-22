0086a033948ca62127621fddf4e7f54cec8a41ad
thoroc <thomas.a.roche@gmail.com>
2025-01-22 09:03:06 +0000

    staging files maybe

--- opt/scripts/commands/extract/command.ts
+++ opt/scripts/commands/extract/command.ts
@@ -1,19 +1,19 @@
-import { Command } from "jsr:@cliffy/command@^1.0.0-rc.7";
-import chalk from "npm:chalk";
-import figlet from "npm:figlet";
+import { Command } from 'jsr:@cliffy/command@^1.0.0-rc.7';
+import chalk from 'npm:chalk';
+import figlet from 'npm:figlet';
 
 interface ExtractCommandOptions {
   output?: string;
 }
 
 export const extractCommand = new Command()
-  .arguments("<source:string>")
-  .description("Extract a single xls file into a json file.")
-  .option("-o, --output <output:string>", "Output path.")
+  .arguments('<source:string>')
+  .description('Extract a single xls file into a json file.')
+  .option('-o, --output <output:string>', 'Output path.')
   .action((options: ExtractCommandOptions, source: string) => {
     console.log(
       chalk.bold.yellow(
-        figlet.textSync("Extract data", { font: "Larry 3D" }),
+        figlet.textSync('Extract data', { font: 'Larry 3D' }),
       ),
     );
 
--- opt/scripts/commands/load/command.ts
+++ opt/scripts/commands/load/command.ts
@@ -1,19 +1,19 @@
-import { Command } from "jsr:@cliffy/command@^1.0.0-rc.7";
-import chalk from "npm:chalk";
-import figlet from "npm:figlet";
+import { Command } from 'jsr:@cliffy/command@^1.0.0-rc.7';
+import chalk from 'npm:chalk';
+import figlet from 'npm:figlet';
 
 interface LoadCommandOptions {
   output?: string;
 }
 
 export const loadCommand = new Command()
-  .arguments("<source:string>")
-  .description("Load a transformed data file.")
-  .option("-o, --output <output:string>", "Output path.")
+  .arguments('<source:string>')
+  .description('Load a transformed data file.')
+  .option('-o, --output <output:string>', 'Output path.')
   .action((options: LoadCommandOptions, source: string) => {
     console.log(
       chalk.bold.yellow(
-        figlet.textSync("Load data", { font: "Larry 3D" }),
+        figlet.textSync('Load data', { font: 'Larry 3D' }),
       ),
     );
 
