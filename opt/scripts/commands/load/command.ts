7581bd32ed74a0cf34c767862ed490f17c0637b2
thoroc <thomas.a.roche@gmail.com>
2025-01-22 08:59:34 +0000

    wiring up the task

--- opt/scripts/commands/load/command.ts
+++ opt/scripts/commands/load/command.ts
@@ -1,14 +1,22 @@
-import { Command } from 'jsr:@cliffy/command@^1.0.0-rc.7';
+import { Command } from "jsr:@cliffy/command@^1.0.0-rc.7";
+import chalk from "npm:chalk";
+import figlet from "npm:figlet";
 
 interface LoadCommandOptions {
   output?: string;
 }
 
 export const loadCommand = new Command()
-  .arguments('<source:string>')
-  .description('Load a transformed data file.')
-  .option('-o, --output <output:string>', 'Output path.')
+  .arguments("<source:string>")
+  .description("Load a transformed data file.")
+  .option("-o, --output <output:string>", "Output path.")
   .action((options: LoadCommandOptions, source: string) => {
+    console.log(
+      chalk.bold.yellow(
+        figlet.textSync("Load data", { font: "Larry 3D" }),
+      ),
+    );
+
     console.log(
       "load command called for '%s' with options: %o",
       source,
--- opt/scripts/commands/transform/command.ts
+++ opt/scripts/commands/transform/command.ts
@@ -1,16 +1,24 @@
-import { Command } from 'jsr:@cliffy/command@^1.0.0-rc.7';
+import { Command } from "jsr:@cliffy/command@^1.0.0-rc.7";
+import chalk from "npm:chalk";
+import figlet from "npm:figlet";
 
 interface TransformCommandOptions {
   output?: string;
 }
 
 export const transformCommand = new Command()
-  .arguments('<source:string>')
-  .description('Transform a raw json file into a clean data json file.')
-  .option('-o, --output <output:string>', 'Output path.')
+  .arguments("<source:string>")
+  .description("Transform a raw json file into a clean data json file.")
+  .option("-o, --output <output:string>", "Output path.")
   .action((options: TransformCommandOptions, source: string) => {
     console.log(
-      'transform command called for %s with options: %o',
+      chalk.bold.yellow(
+        figlet.textSync("Transform data", { font: "Larry 3D" }),
+      ),
+    );
+
+    console.log(
+      "transform command called for %s with options: %o",
       source,
       options,
     );
