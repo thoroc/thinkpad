b01e413485804f4dff48bd5b1bf6701e0dc3c0fe
thoroc <thomas.a.roche@gmail.com>
2025-07-21 09:28:04 +0100

    feat(transform): update transformAction to use TransformOptions

--- opt/scripts/commands/transform/action/action.ts
+++ opt/scripts/commands/transform/action/action.ts
@@ -1,23 +1,28 @@
 import chalk from 'npm:chalk';
 import figlet from 'npm:figlet';
+import { transformFile } from './file.ts';
+import { TransformOptions } from './types.ts';
 
-interface TransformActionOptions {
-  output?: string;
-}
-
-export const transformAction = (
-  options: TransformActionOptions,
+export const transformAction = async (
+  options: TransformOptions,
   source: string,
 ) => {
+  console.log(
+    "transform command called for '%s' with options: %o",
+    source,
+    options,
+  );
   console.log(
     chalk.bold.yellow(
       figlet.textSync('Transform data', { font: 'Larry 3D' }),
     ),
   );
 
-  console.log(
-    "transform command called for '%s' with options: %o",
-    source,
-    options,
-  );
+  const stat = await Deno.stat(source);
+
+  if (stat.isDirectory) {
+    console.log(chalk.red('Directory transformation is not supported yet.'));
+  } else {
+    await transformFile(source, options);
+  }
 };
