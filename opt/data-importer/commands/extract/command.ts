b2cfb65b3c0cf783cdb40e5e6684fd9694a8855d
thoroc <thomas.a.roche@gmail.com>
2025-08-02 19:42:47 +0100

    refactor(command): simplify outputDir option definition

--- opt/data-importer/commands/extract/command.ts
+++ opt/data-importer/commands/extract/command.ts
@@ -1,18 +1,15 @@
 import { Command } from 'jsr:@cliffy/command@^1.0.0-rc.8';
-import { toString } from '../utils/command-option.ts';
 import { extractAction } from './action/mod.ts';
-
-const outputDirOption = toString({
-  flag: {
-    key: 'outputDir',
-    value: 'string',
-  },
-  description: 'Output directory for extracted data.',
-});
 
 export const extractCommand = new Command()
   .alias('e')
   .arguments('<source:string>')
   .description('Extract a single xls file into a json file.')
-  .option(outputDirOption.flag, outputDirOption.description)
+  .option(
+    '-o, --outputDir <dir:string>',
+    'Output directory for extracted data.',
+    {
+      default: `${Deno.cwd()}/data/extracted`,
+    }
+  )
   .action(extractAction);
